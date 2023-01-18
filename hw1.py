628a6ce35a4f9661b4b4d5c851620ebeb2485288
Maksym Shulha <maxshulga94@gmail.com>
2023-01-18 14:50:04 +0200

    my hw

--- hw1.py
+++ hw1.py
@@ -24,28 +24,28 @@ class Group:
     def __init__(self, specialize):
         self.specialize = specialize
         self.list_group = []
+        self.finded_st = []
 
     def add_st(self, student):
-        self.list_group.append(student)
-        if len(self.list_group) > 10:
-            del_student = input('Enter surname, name, age')
-            self.list_group.remove(del_student)
+        if len(self.list_group) <= 9:
+            self.list_group.append(student)
 
     def plus_st(self):
         new_student = {'inform': input('Enter surname, name and age: ')}
         self.list_group.append(new_student.get('inform'))
 
     def del_st(self):
-        if len(self.list_group) > 10:
-            del_student = input('Enter surname, name, age')
-            self.list_group = self.list_group.remove(del_student)
+        del_student = input('Enter surname, name, age: ')
+        if del_student in self.list_group:
+            self.list_group.remove(del_student)
 
-    #def search_st(self):
-    #   s_student = input('Enter surname: ')
-    #    for s_student in self.list_group:
+    def search_st(self):
+        search_student = input('Enter surname, name, age: ')
+        if search_student in self.list_group:
+            self.finded_st.append(search_student)
 
     def __str__(self):
-        return f'{self.specialize}:\n' + '\n'.join(map(str, self.list_group))
+        return f'{self.specialize}:\n' + '\n'.join(map(str, self.list_group)) + {self.finded_st}
 
 
 gr = Group('Python-pro')
@@ -60,6 +60,7 @@ st7 = Student('Petrenko', 'Ivan', '48')
 st8 = Student('Maksymenko', 'Ivan', '23')
 st9 = Student('Ivanenko', 'Ivan', '17')
 st10 = Student('Borysenko', 'Ivan', '35')
+st11 = Student('Borko', 'Ivan', '35')
 
 gr.add_st(st1)
 gr.add_st(st2)
@@ -70,9 +71,10 @@ gr.add_st(st6)
 gr.add_st(st7)
 gr.add_st(st8)
 gr.add_st(st9)
-gr.add_st(st10)
-gr.add_st(st10)
+# gr.add_st(st10)
+# gr.add_st(st11)
 
-#gr.plus_st()
-gr.del_st()
+# gr.plus_st()
+#gr.del_st()
+gr.search_st()
 print(gr)
