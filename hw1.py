44664f5769cdacc925113980fa906ccda610cd28
Maksym Shulha <maxshulga94@gmail.com>
2023-01-20 23:32:53 +0200

    20.01.23 new hw

--- hw1.py
+++ hw1.py
@@ -2,12 +2,12 @@ import logging
 
 logger = logging.getLogger('Add_student')
 logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
+inf = logging.StreamHandler()
+inf.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
+inf.setFormatter(formatter)
+logger.addHandler(inf)
 
 
 class LimitError(Exception):
