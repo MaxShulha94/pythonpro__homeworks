566d6900403a1a9be2b2116b85230ee145063609
MaxShulha94 <maxshulga94@gmail.com>
2023-01-29 16:31:03 +0200

    HW29.01

---
+++ hw_fraction.py
@@ -0,0 +1,57 @@
+import math
+from math import gcd
+
+
+class Fraction:
+
+    def __init__(self, numerator: int, denumerator: int):
+        self.numerator = numerator
+        self.denumerator = denumerator
+
+    def __gt__(self, other):
+        return Fraction(self.numerator > other.numerator, self.denumerator > other.denumerator)
+
+    def __ge__(self, other):
+        return Fraction(self.numerator >= other.numerator, self.denumerator >= other.denumerator)
+
+    def __lt__(self, other):
+        return Fraction(self.numerator < other.numerator, self.denumerator < other.denumerator)
+
+    def __le__(self, other):
+        return Fraction(self.numerator <= other.numerator, self.denumerator <= other.denumerator)
+
+    def __eq__(self, other):
+        return Fraction(self.numerator == other.numerator, self.denumerator == other.denumerator)
+
+    def __ne__(self, other):
+        return Fraction(self.numerator != other.numerator, self.denumerator != other.denumerator)
+
+    def __mul__(self, other):
+        return Fraction(self.numerator * other.numerator, self.denumerator * other.denumerator)
+
+    def __add__(self, other):
+        return Fraction((self.numerator * other.denumerator) + (other.numerator * self.denumerator),
+                        (self.denumerator * other.denumerator))
+
+    def __sub__(self, other):
+        return Fraction((self.numerator * other.denumerator) - (other.numerator * self.denumerator),
+                        (self.denumerator * other.denumerator))
+
+    def __floordiv__(self, other):
+        return Fraction(self.numerator * other.denumerator, self.denumerator * other.numerator)
+
+    def __str__(self):
+        if self.numerator == self.denumerator:
+            return f'1'
+        if self.numerator < self.denumerator:
+            return f'{self.numerator // gcd(self.numerator, self.denumerator)}/{self.denumerator // gcd(self.numerator, self.denumerator)}'
+        if self.numerator > self.denumerator:
+            self.numerator = self.numerator // gcd(self.numerator, self.denumerator)
+            self.denumerator = self.denumerator // gcd(self.numerator, self.denumerator)
+            intgr = self.numerator // self.denumerator
+            new_numerator = self.numerator - (intgr * self.denumerator)
+            return f'{intgr} {new_numerator}/{self.denumerator}'
+
+a = Fraction(1, 6)
+b = Fraction(9, 5)
+print(b.__sub__(a))
---
+++ hw_rectangle.py
@@ -0,0 +1,38 @@
+class Rectangle:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def __square(self):
+        return self.height * self.width
+
+    def __add__(self, other):
+        return self.__square() + other.__square()
+
+    def mul_n(self, n):
+        return self.__square() * n
+
+    def __eq__(self, other):
+        return self.__square() == other.__sqare()
+
+    def __ne__(self, other):
+        return self.__square() != other.__sqare
+
+    def __lt__(self, other):
+        return self.__square() < other.__sqare()
+
+    def __le__(self, other):
+        return self.__square() <= other.__sqare()
+
+    def __gt__(self, other):
+        return self.__square() > other.__sqare()
+
+    def __ge__(self, other):
+        return self.__square() >= other.__sqare
+
+    def __str__(self):
+        return f'{self.width} * {self.height} = {self.__square()}'
+
+a = Rectangle(5, 5)
+b = Rectangle(4, 4)
+print(a)
