02d24e03ae5b171bbcb1177a03fff6975a3a3f43
MaxShulha94 <maxshulga94@gmail.com>
2023-02-04 12:51:00 +0200

    HW-04.02

--- pizza_order.py
+++ pizza_order.py
@@ -8,33 +8,35 @@ class Customer:
     def __str__(self):
         return f'{self.surname} {self.name[0]}.; {self.phone}'
 
+
 class Pizza:
 
     def __init__(self):
-
         self.pizza_dict = {'Monday': 'Cheese Pizza', 'Tuesday': 'Pepperoni Pizza', 'Wednesday': 'Meat Pizza',
                            'Thursday': 'Margherita Pizza', 'Friday': 'Hawaiian Pizza', 'Saturday': 'The Works Pizza',
                            'Sunday': 'Sicilian Pizza'}
         self.price_dict = {'Monday': 100, 'Tuesday': 95, 'Wednesday': 110, 'Thursday': 90, 'Friday': 95, 'Saturday': 90,
-                      'Sunday': 100}
+                           'Sunday': 100}
         self.today = input('What day is it today?')
         self.pizza_of_the_day = self.pizza_dict[self.today]
         self.price_of_pizza = self.price_dict[self.today]
+
     def __str__(self):
         return f'{self.pizza_of_the_day}: {self.price_of_pizza} UAH'
 
+
 class Order:
 
+    def __init__(self, customer: Customer, pizza_of_the_day):
 
-    def __init__(self,  customer: Customer, pizza_of_the_day: Pizza):
 
         self.customer = customer
         self.orders = []
         self.__quantities = []
 
-    def add_product(self, quantity: int | float = 1):
-            self.orders.append(self.pizza_of_the_day)
-            self.__quantities.append(quantity)
+    def add_product(self, pizza: Pizza, quantity: int | float = 1):
+        self.orders.append(pizza)
+        self.__quantities.append(quantity)
 
     def total(self):
         result = 0
