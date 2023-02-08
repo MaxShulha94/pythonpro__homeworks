b3eb18f66a60cbb50fe1eee0928411a7caa027f3
MaxShulha94 <maxshulga94@gmail.com>
2023-02-08 16:35:22 +0200

    HW-08.02

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+def fib():
+    buf = [0, 1]
+    prev, curr = 0, 1
+
+    def get_next(n):
+        nonlocal prev, curr
+        while len(buf) < n:
+            prev, curr = curr, prev + curr
+            buf.append(curr)
+        if len(buf) >= n:
+            return buf[n]
+
+        res = buf[-1]
+        for i in range(len(buf), n):
+            prev, curr = curr, prev + curr
+            buf.append(curr)
+
+        return res
+
+    return get_next
+
+
+x = fib()
+print(x(4))
