ff0587c71e6f127da57fe00b04a1ad04417ca12f
MaxShulha94 <maxshulga94@gmail.com>
2023-02-08 23:05:16 +0200

    HW-08.02

--- fibonacci.py
+++ fibonacci.py
@@ -1,24 +1,36 @@
-def fib():
-    buf = []
-    prev, curr = 0, 1
-
-    def get_next(n):
-        nonlocal prev, curr
-        while len(buf) < n:
-            prev, curr = curr, prev + curr
-            buf.append(curr)
-        if len(buf) >= n:
-            return buf[n]
-
-        res = buf[-1]
-        for i in range(len(buf), n):
-            prev, curr = curr, prev + curr
-            buf.append(curr)
-
-        return res
-
-    return get_next
+import timeit
 
 
-x = fib()
-print(x(4))
+stmt_closure = """
+def fibonacci():
+    res = [0, 1]
+
+    def get_value(n):
+        if len(res) > n:
+            return res[n]
+
+        prev, cur = res[-2], res[-1]
+        while len(res) <= n:
+            prev, cur = cur, cur + prev
+            res.append(cur)
+        return res[-1]
+
+    return get_value
+f = fibonacci()
+f(40)
+"""
+
+
+stmt_recursion = """
+import functools
+@functools.lru_cache
+def recursion_fibonacci(n):
+    if n <= 1:
+        return n
+    return recursion_fibonacci(n-1) + recursion_fibonacci(n-2)
+recursion_fibonacci(40)
+"""
+
+
+print(timeit.timeit(stmt_closure, number=10))
+print(timeit.timeit(stmt_recursion, number=10))
