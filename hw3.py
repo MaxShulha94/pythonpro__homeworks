99539fd3306f35b13b35f2933e81fb9da24ffe5b
MaxShulha94 <maxshulga94@gmail.com>
2023-02-23 00:18:10 +0200

    23.02

--- hw3.py
+++ hw3.py
@@ -6,7 +6,7 @@ class Box:
 
     @staticmethod
     def vol(box_1, box_2):
-        print(f'Sum volume = {box_1 + box_2}')
+        return f'Sum volume = {box_1 + box_2}'
 
 
 Box.vol(5, 5)
@@ -15,9 +15,6 @@ Box.vol(5, 5)
 
 
 class Descriptor:
-
-    def __init__(self, some_inst):
-        self.some_inst = some_inst
 
     def __get__(self, instance, owner):
         return self.some_inst
@@ -29,33 +26,32 @@ class Descriptor:
         raise AttributeError('Can not delete')
 
 
+
 """5. Реалізуйте функціонал, який заборонятиме встановлення полів класу будь-якими значеннями, крім цілих чисел."""
 
 
 class Num:
 
+
     def __init__(self, var: int | float):
         self.var = var
 
-    var = property()
 
-    @var.getter
-    def var(self):
-        if isinstance(self.var, int | float):
-            return self.var
+    def __get__(self, instance, owner):
+        if isinstance(instance, int | float):
+            return instance.__dict__[self._var]
+        else:
+            raise TypeError('instance must be int')
+
+
+    def __set__(self, instance, value):
+        if isinstance(instance, int | float):
+            instance.__dict__[self._var] = value
         raise TypeError('instance must be int')
 
-    @var.setter
-    def var(self, new_var):
-        if isinstance(new_var, int | float):
-            self.var = new_var
-        raise TypeError('instance must be int')
-
-    @var.deleter
-    def var(self):
-        del self.var
 
 
-num_1 = Num(123)
+
+num_1 = Num('1')
 
 print(num_1.var)
